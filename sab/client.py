086a1e86b9f6f55b27e33b3cf147ef1625d4e866
Julie Luciani <119424712+Jyaax@users.noreply.github.com>
2024-04-26 18:40:39 +0200

    Update client.py

--- sab/client.py
+++ sab/client.py
@@ -6,7 +6,7 @@ def main():
     client_socket = socket.socket()
     client_socket.connect((host, port))
     
-    response = client_socket.recv(50000).decode()
+    response = client_socket.recv(300000).decode()
     print(response)
     
     client_socket.close()
