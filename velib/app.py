08243e9e146fc6115b5f3330601d32dc6ecd0f6a
Myaki <lili_yang@hotmail.fr>
2024-04-27 00:37:40 +0200

    add work

--- velib/app.py
+++ velib/app.py
@@ -3,41 +3,63 @@ import mysql.connector
 
 app = Flask(__name__)
 
-connection = mysql.connector.connect(
-    host="hostname",
-    user='root',
-    password='',
-    database='velib'
-)
-cursor = connection.cursor()
+@app.route('/')
+def index():
+    return "<p>Bienvenue</p>"
 
-@app.route('/favorites', methods=['POST'])
+def get_database_connection():
+    return mysql.connector.connect(
+        user='root',
+        password='',
+        database='velib'
+    )
+
+def execute_query(query, data=None):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    try:
+        if data:
+            cursor.execute(query, data)
+        else:
+            cursor.execute(query)
+        connection.commit()
+        return cursor
+    except:
+        connection.rollback()
+        raise
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/favoris', methods=['POST'])
 def add_favorite():
     data = request.json
-    user_id = data.get('user_id')
-    favorite_data = data.get('favorite_data')
+    user_id = data.get('id_user')
+    nom = data.get('nom')
+    adresse = data.get('adresse')
+    capacite = data.get('capacite')
+    code = data.get('code')
+    paiement = data.get('paiement')
 
-    query = "INSERT INTO favorites (user_id, favorite_data) VALUES (%s, %s)"
-    cursor.execute(query, (user_id, favorite_data))
-    connection.commit()
+    query = "INSERT INTO favoris (id_user, nom, adresse, capacite, code, paiement) VALUES (%s, %s, %s, %s, %s, %s)"
+    execute_query(query, (user_id, nom, adresse, capacite, code, paiement))
 
     return jsonify({"message": "Favorite ajouté avec succès"})
 
-@app.route('/favorites/<int:favorite_id>', methods=['DELETE'])
-def delete_favorite(favorite_id):
-    query = "DELETE FROM favorites WHERE id = %s"
-    cursor.execute(query, (favorite_id,))
-    connection.commit()
+@app.route('/favoris/<int:id_favoris>', methods=['DELETE'])
+def delete_favorite(id_favoris):
+    query = "DELETE FROM favoris WHERE id_favoris = %s"
+    execute_query(query, (id_favoris,))
 
     return jsonify({"message": "Favori supprimé avec succès"})
 
-@app.route('/favorites/<int:user_id>', methods=['GET'])
-def get_user_favorites(user_id):
-    query = "SELECT * FROM favorites WHERE user_id = %s"
-    cursor.execute(query, (user_id,))
-    favorites = cursor.fetchall()
-
-    return jsonify(favorites)
+@app.route('/favoris/<int:user_id>', methods=['GET'])
+def get_user_favoris(user_id):
+    query = "SELECT * FROM favoris WHERE id_user = %s"
+    cursor = execute_query(query, (user_id,))
+    favoris = cursor.fetchall() 
+    cursor.close()
+    return jsonify(favoris)
 
 if __name__ == "__main__":
     app.run(debug=True)
