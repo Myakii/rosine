f080bf493bb390a437f1c9ac49346ae7894d710a
Myaki <lili_yang@hotmail.fr>
2024-04-26 22:30:18 +0200

    favoris

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from database import connect_to_database 
+import json
+
+app = Flask(__name__)
+
+connection = connect_to_database()
+cursor = connection.cursor();
+
+@app.route('/favorites', methods=['POST'])
+def add_favorite():
+    data = request.json
+    user_id = data.get('user_id')
+    favorite_data = data.get('favorite_data')
+
+    query = "INSERT INTO favorites (user_id, favorite_data) VALUES (%s, %s)"
+    cursor.execute(query, (user_id, favorite_data))
+    connection.commit()
+
+    return jsonify({"message": "Favorite ajouté avec succès"})
+
+@app.route('/favorites/<int:favorite_id>', methods=['DELETE'])
+def delete_favorite(favorite_id):
+    query = "DELETE FROM favorites WHERE id = %s"
+    cursor.execute(query, (favorite_id,))
+    connection.commit()
+
+    return jsonify({"message": "Favori supprimé avec succès"})
+
+@app.route('/favorites/<int:user_id>', methods=['GET'])
+def get_user_favorites(user_id):
+    query = "SELECT * FROM favorites WHERE user_id = %s"
+    cursor.execute(query, (user_id,))
+    favorites = cursor.fetchall()
+
+    return jsonify(favorites)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,54 @@
+from flask import Flask;
+import mysql.connector
+
+app = Flask(__name__)
+
+def connect_to_database():
+    connection = mysql.connector.connect(
+        host="hostname",
+        user='root',
+        password='',
+        database='velib'
+    )
+    return connection
+
+def insert_favorite(user_id, favorite_data):
+
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "INSERT INTO favorites (user_id, favorite_data) VALUES (%s, %s)"
+    cursor.execute(query, (user_id, favorite_data))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def delete_favorite(user_id, favorite_id):
+
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "DELETE FROM favorites WHERE user_id = %s AND id = %s"
+    cursor.execute(query, (user_id, favorite_id))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def get_user_favorites(user_id):
+
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM favorites WHERE user_id = %s"
+    cursor.execute(query, (user_id,))
+    favorites = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return favorites
+
+if __name__ == "__main__":
+    app.run(debug=True)
