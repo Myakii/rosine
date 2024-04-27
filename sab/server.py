08ec670bab5919b759719a01ef833e4c9b420e94
Julie Luciani <119424712+Jyaax@users.noreply.github.com>
2024-04-27 10:30:50 +0200

    Update server.py

--- sab/server.py
+++ sab/server.py
@@ -4,29 +4,43 @@ import time
 import requests
 
 class VelibData:
-    #utilisation d'une classe pour une question de clead code
+    #Utilisation d'une classe pour une question de clean code
     def __init__(self):
         self.last_request_time = 0
         self.data = None
     
     def fetch_data(self):
-        #comparaison en amont avec la date du dernier appel, s'il date de moins de 300s, on retourne data
+        #Comparaison en amont avec la date du dernier appel, s'il date de moins de 300s, on retourne data sans appeler l'api
         current_time = time.time()
         if current_time - self.last_request_time < 300 and self.data:
             return self.data
-        #utilisation de l'opendata de Velib et non Paris car celui de Paris bloquait les requetes à 100 stations
-        api_url = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"
-        #sinon, on appelle l'api, stocke la date actuelle puis les données récupérées
-        try:
-            response = requests.get(api_url)
-            if response.status_code == 200:
-                self.last_request_time = current_time
-                self.data = response.json()
-                return self.data
-        except:
-            print("Erreur lors de la récupération des données Velib.")
         
-        return None
+        #Pagination pour récupérer toutes les données Velib
+        all_data = []
+        total_results = 1468
+        limit = 100
+        offset = 0
+
+        #Boucle pour avoir toutes les données des vélib, la limite étant à 100 et le max 1468
+        while offset < total_results:
+            api_url = f"https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit={limit}&offset={offset}"
+            try:
+                response = requests.get(api_url)
+                if response.status_code == 200:
+                    data = response.json()
+                    all_data.extend(data["results"])  #Ajout des données des différents appels dans 1 variable
+                    offset += limit
+                else:
+                    print(f"Erreur lors de la récupération des données Velib: {response.status_code}")
+                    return None
+            except Exception as e:
+                print(f"Erreur lors de la récupération des données Velib: {e}")
+                return None
+            
+        #Mise à jour de l'heure du dernier appel et de la liste des données
+        self.last_request_time = current_time
+        self.data = all_data
+        return self.data
 
 try:
     velib_data = VelibData()
@@ -43,11 +57,12 @@ try:
         if data:
             socket_client.sendall(json.dumps(data).encode())
         else:
-            socket_client.sendall("Erreur lors de la récupération des données Velib.")
+            #Conversion de la chaîne en bytes avant l'envoi
+            socket_client.sendall("Erreur lors de la récupération des données Velib.".encode())
         socket_client.close()
-except KeyboardInterrupt: #Pas forcément utile mais c'est apparemment une bonne pratique
+except KeyboardInterrupt:
     print("Interruption du serveur par l'utilisateur.")
-except Exception:
-    print("Erreur lors de l'exécution du serveur.")
+except Exception as e:
+    print(f"Erreur lors de l'exécution du serveur: {e}")
 finally:
     socket_server.close()
