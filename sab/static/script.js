d9cbbe58479c38cd454521c74c1f08d157c4b9d5
Julie Luciani <119424712+Jyaax@users.noreply.github.com>
2024-04-27 09:30:14 +0200

    Update script.js

--- sab/static/script.js
+++ sab/static/script.js
@@ -1,33 +1,33 @@
-const map = L.map('map').setView([48.8566, 2.3522], 13);
+//execution de fetchVelibData au chargement de la page pour les afficher directement sur la map
+window.addEventListener("load", fetchVelibData);
 
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        minZoom: 13,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+const map = L.map("map").setView([48.8566, 2.3522], 13);
 
-    //L.marker([48.8584, 2.2945]).addTo(map).bindPopup('Tour Eiffel');
+const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  minZoom: 13,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
-    function fetchVelibData() {
-        fetch('http://localhost:3000')
-            .then(data => {
-		console.log(data)
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des données Velib.');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data); 
-                data.data.stations.forEach(station => {
-                    const lat = station.lat;
-                    const lon = station.lon;
-                    console.log(lat, lon); 
-    
-                    L.marker([lat, lon]).addTo(map);
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-    
+function fetchVelibData() {
+  //on récupère les données à l'url définie dans app.py
+  fetch("http://localhost:5000/velib")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des données Velib.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.data.stations.forEach((station) => {
+        const lat = station.lat;
+        const lon = station.lon;
+        console.log(lat, lon);
+        L.marker([lat, lon]).addTo(map);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
