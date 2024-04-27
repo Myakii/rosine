f291c88d76f40d22d37cc7fd49eb38991325dc87
Julie Luciani <119424712+Jyaax@users.noreply.github.com>
2024-04-27 09:27:47 +0200

    Update app.py

--- sab/app.py
+++ sab/app.py
@@ -1,7 +1,28 @@
-from flask import Flask 
-from flask import render_template
-app = Flask("mon app")
+from flask import Flask, render_template, jsonify
+from server import VelibData
+
+app = Flask(__name__)
+velib_data = VelibData()
 
 @app.route("/")
 def accueil():
     return render_template("index.html")
+
+#récupération des données à une autre route en important la variable depuis server.py, lancé en amont et qui aura récupéré les données de l'api
+@app.route("/velib")
+def velib():
+    data = get_velib_data()
+    response = jsonify(data)
+    #ajout des headers pour les autorisations CORS
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+def get_velib_data():
+    data = velib_data.fetch_data()
+    if data:
+        return data
+    else:
+        return {"error": "Erreur lors de la récupération des données Velib."}
+
+if __name__ == "__main__":
+    app.run(debug=True)
